f6a19106c132768c5307fa9048876b0fb5d5c910
Riddhi K Singh <Riddhi.Singh@in.ey.com>
2025-06-23 09:42:59 +0530

    push

--- app/main.py
+++ app/main.py
@@ -1,27 +1,27 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.trustedhost import TrustedHostMiddleware
-import os
+from neo4j import AsyncGraphDatabase
+
 from app.api.api_router import api_router
 from app.core.config import get_settings
-from neo4j import AsyncGraphDatabase
+
+
 app = FastAPI(
     title="minimal fastapi postgres template",
     version="6.1.0",
     description="https://github.com/20230028426_EYGS/coe-ens-analysis-orchestration.git",
-    openapi_url="/openapi.json",
     docs_url="/",
+    root_path="/api/orchestration"
 )
 
 app.include_router(api_router)
-print("GRAPHDB__URI", os.environ.get("GRAPHDB__URI"))
+
 # Sets all CORS enabled origins
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        str(origin).rstrip("/")
-        for origin in get_settings().security.backend_cors_origins
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -30,7 +30,7 @@ app.add_middleware(
 # Guards against HTTP Host Header attacks
 app.add_middleware(
     TrustedHostMiddleware,
-    allowed_hosts=get_settings().security.allowed_hosts,
+    allowed_hosts=["*"],
 )
 
 @app.on_event("startup")
